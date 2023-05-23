99a4753e54253ec57b3d4e163349b5c945cb5dae
Gleb-Ablyaev <99827453+Gleb-Ablyaev@users.noreply.github.com>
2023-05-23 15:26:17 +0400

    Add files via upload

---
+++ WebApplication1/Controllers/CalculatorController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            List<string> history = new List<string>();
+
+            if (Request.QueryString["history"] != null)
+            {
+                history = Request.QueryString["history"].Split(',').ToList();
+            }
+
+            ViewBag.History = history;
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Calculate(CalculatorModel model, string action)
+        {
+            bool status = false;
+
+            if (action == "calculate")
+            {
+                if (ModelState.IsValid)
+                {
+                    switch (model.Operation)
+                    {
+                        case "+":
+                            model.Result = model.FstNumber + model.SndNumber;
+                            break;
+                        case "-":
+                            model.Result = model.FstNumber - model.SndNumber;
+                            break;
+                        case "/":
+                            if (model.SndNumber == 0)
+                            {
+                                ViewBag.Message = "Нельзя делить на 0!";
+                                status = true;
+                            }
+                            else
+                            {
+                                model.Result = model.FstNumber / model.SndNumber;
+                            }
+                            break;
+                        case "*":
+                            model.Result = model.FstNumber * model.SndNumber;
+                            break;
+                    }
+
+                    if (status == false)
+                    {
+                        ViewBag.Message = "Вычисление произведено успешно!";
+                    }
+
+                    // Создаем строку истории
+                    string historyItem = $"{model.FstNumber},{model.Operation},{model.SndNumber},{model.Result}";
+
+                    // Получаем текущую историю из QueryString
+                    string currentHistory = Request.QueryString["history"];
+
+                    // Объединяем текущую историю с новым элементом истории
+                    string updatedHistory = string.IsNullOrEmpty(currentHistory) ? historyItem : currentHistory + "," + historyItem;
+
+                    // Передаем историю через QueryString
+                    return RedirectToAction("Index", new { history = updatedHistory });
+                }
+                else
+                {
+                    // Получаем текущую историю из QueryString
+                    string currentHistory = Request.QueryString["history"];
+
+                    // Создаем строку истории
+                    string historyItem = $"{model.FstNumber},{model.Operation},{model.SndNumber},{model.Result}";
+
+                    // Объединяем текущую историю с новым элементом истории
+                    string updatedHistory = string.IsNullOrEmpty(currentHistory)
+                        ? historyItem
+                        : currentHistory + "," + historyItem;
+
+                    // Получаем текущий URL
+                    string currentUrl = Request.Url.ToString();
+
+                    // Определяем позицию символа '?' в URL
+                    int queryStringIndex = currentUrl.LastIndexOf('?');
+
+                    // Обновляем URL с новым QueryString
+                    string updatedUrl = queryStringIndex != -1
+                        ? currentUrl.Substring(0, queryStringIndex + 1) + "history=" + HttpUtility.UrlEncode(updatedHistory)
+                        : currentUrl + "?history=" + HttpUtility.UrlEncode(updatedHistory);
+
+                    // Перенаправляем на обновленный URL
+                    return Redirect(updatedUrl);
+                }
+
+                
+            }
+            else if (action == "clear")
+            {
+                // Очищаем историю
+                return RedirectToAction("Index", new { history = "" });
+            }
+            else
+            {
+                return View("Index", model);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Result()
+        {
+            List<string> history = new List<string>();
+
+            if (Request.QueryString["history"] != null)
+            {
+                history = Request.QueryString["history"].Split(',').ToList();
+            }
+
+            return View(history);
+        }
+    }
+}
---
+++ WebApplication1/Models/CalculatorModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class CalculatorModel
+    {
+        [Required(ErrorMessage = "FirstNumber является обязательным.")]
+        public int FstNumber { get; set; }
+        public int FstNumber2 { get; set; }
+
+        [StringLengthWrapper(5,  1, ErrorMessage = "Длина числа должна быть от 1 до 5 цифр.")]
+        public int SndNumber { get; set; }
+
+        public string Operation { get; set; }
+        public float Result { get; set; }
+    }
+
+    public class StringLengthWrapperAttribute : ValidationAttribute
+    {
+        private readonly int _maxLength;
+        private readonly int _minLength;
+
+        public StringLengthWrapperAttribute(int maxLength)
+        {
+            _maxLength = maxLength;
+        }
+
+        public StringLengthWrapperAttribute(int maxLength, int minLength) : this(maxLength)
+        {
+            _minLength = minLength;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value != null)
+            {
+                string stringValue = value.ToString();
+                if (stringValue.Length < _minLength || stringValue.Length > _maxLength)
+                {
+                    return new ValidationResult(ErrorMessage);
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
